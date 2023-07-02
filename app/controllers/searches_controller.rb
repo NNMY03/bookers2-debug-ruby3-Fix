8a0e61ec9ff8bc803bc6c862b44d49c831318916
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-07-02 15:53:16 +0000

    [Add]検索フォーム作成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,13 @@
 class SearchesController < ApplicationController
   def search
+    @range = params[:range] #検索モデル
+    @word = params[:word]   #検索ワード
+   if @range == "User"
+    @users = User.looks(params[:search], params[:word])#[:seach]=検索方法
+    #@usersにUserモデル内の検索結果を代入する
+   else
+    @books = Book.looks(params[:search], params[:word])#looksメソッドは検索内容の取得
+    #@booksにBookモデル内の検索結果を代入する
+   end
   end
 end
