d17a6361d6c059fcec0619bbdb20153251f3a4da
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-07-06 17:41:30 +0000

    [Add]５段階評価追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -61,6 +61,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :comment)
+    params.require(:book).permit(:title, :body, :comment, :star)
   end
 end
